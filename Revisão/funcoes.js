f131e2ac09674eedce1e4b5928a87ef0cdb027ee
Luisa Luz <83250828+LouWilly@users.noreply.github.com>
2022-02-21 21:44:03 -0300

    Add files via upload

---
+++ Revisão/funcoes.js
@@ -0,0 +1,64 @@
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+// Usage
+var myEfficientFn = debounce(function() {
+	// All the taxing stuff you do
+}, 250);
+window.addEventListener('resize', myEfficientFn);
+
+function poll(fn, callback, errback, timeout, interval) {
+    var endTime = Number(new Date()) + (timeout || 2000);
+    interval = interval || 100;
+
+    (function p() {
+            // If the condition is met, we're done! 
+            if(fn()) {
+                callback();
+            }
+            // If the condition isn't met but the timeout hasn't elapsed, go again
+            else if (Number(new Date()) < endTime) {
+                setTimeout(p, interval);
+            }
+            // Didn't match and too much time, reject!
+            else {
+                errback(new Error('timed out for ' + fn + ': ' + arguments));
+            }
+    })();
+}
+
+// Usage:  ensure element is visible
+poll(
+    function() {
+        return document.getElementById('lightbox').offsetWidth > 0;
+    },
+    function() {
+        // Done, success callback
+    },
+    function() {
+        // Error, failure callback
+    }
+);
+
+function matchesSelector(el, selector) {
+	var p = Element.prototype;
+	var f = p.matches || p.webkitMatchesSelector || p.mozMatchesSelector || p.msMatchesSelector || function(s) {
+		return [].indexOf.call(document.querySelectorAll(s), this) !== -1;
+	};
+	return f.call(el, selector);
+}
+
+// Usage
+matchesSelector(document.getElementById('myDiv'), 'div.someSelector[some-attribute=true]')
